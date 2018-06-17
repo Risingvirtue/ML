50c8cb01d5a083422191eec8328aec2917c5790a
Risingvirtue <risingvirtue@berkeley.edu>
2018-06-17 12:35:41 -0700

    draw

--- Perceptron1/sketch.js
+++ Perceptron1/sketch.js
@@ -1,6 +1,13 @@
+var width = 500;
+var height = 500;
+var points = new Array(100);
 function setup() {
-	createCanvas(200, 200);
+	createCanvas(500, 500);
 	var p = new Perceptron(2, 0.001);
+	for (var i =0; i< points.length;i++) {
+		points[i] = new Point();
+	}
+	
 	var inputs = [-1, 0.5];
 	var guess = p.guess(inputs);
 	console.log(guess);
@@ -8,5 +15,10 @@ function setup() {
 }
 
 function draw() {
-	
+	//background(255);
+	stroke(0);
+	line(0,0,width, height);
+	points.forEach(function(p) {
+		p.show();
+	}) 
 }
